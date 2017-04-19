70547b5e9a4d54e917a7767402640a040d5027e0
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-04-19 09:27:07 -0400

    A6Q1-completed with comments

--- src/A6Q1.java
+++ src/A6Q1.java
@@ -34,22 +34,20 @@ public class A6Q1 {
         for (int i = 0; i < marks.length; i++) {
             marks[i] = input.nextInt();
         }
-        
+
         //calculate the sum of the marks to use for calculating the class average
         //create an accumulator
         double sum = 0;
         //go through each number
-        for (int i = 0; i<marks.length;i++){
+        for (int i = 0; i < marks.length; i++) {
             //take input and add on to the current sum
             sum = sum + marks[i];
         }
-        System.out.println("The sum is " + sum +".");
-             
-        //calculate the average
-        double avg = sum/marks.length;
-       
-        System.out.println("The class average is " +avg+"%." );
-                     
+        System.out.println("The sum is " + sum + ".");
 
+        //calculate the average
+        double avg = sum / marks.length;
+        avg = Math.round(avg * 100) / 100.0;
+        System.out.println("The class average is " + avg + "%.");
     }
 }
