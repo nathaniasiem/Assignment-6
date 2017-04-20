fcd52b7851edbec9c6c3eacdfb1aa57e9579d398
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-04-20 09:48:28 -0400

    A6Q3-modified

---
+++ src/A6Q3.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author simon7323
+ */
+public class A6Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan in input
+        Scanner input = new Scanner(System.in);
+        
+        //ask user to input 2 numbers
+        System.out.println("Enter 2 integers: ");
+        
+        //scan input into variable
+        int number = input.nextInt();
+        
+        //create list of numbers
+        int[] integers= new int[number];
+        
+        
+
+
+    }
+}
