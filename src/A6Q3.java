e5c51a2b8eb450676da2d8023056817b6a0edf57
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-04-21 09:53:13 -0400

    A6Q3-completed with comments

--- src/A6Q3.java
+++ src/A6Q3.java
@@ -17,20 +17,29 @@ public class A6Q3 {
     public static void main(String[] args) {
         // create scanner to scan in input
         Scanner input = new Scanner(System.in);
-        
+
+        //create list of numbers
+        int[] integers = new int[2];
+
         //ask user to input 2 numbers
         System.out.println("Enter 2 integers: ");
-        
+
         //scan input into variable
-        int number = input.nextInt();
-        
-        //create list of numbers
-        int[] integers= new int[number];
-        
-        for(int i=0;i<integers.length;i++){
-            
+        for (int i = 0; i < integers.length; i++) {
+            integers[i] = input.nextInt();
         }
-
-
+        
+        //condition to compare numbers of which is larger in order to swap positions
+        if (integers[0] > integers[1]) {
+            int temp = integers[0];
+            integers[0] = integers[1];
+            integers[1] = temp;
+        }
+        
+        //print out numbers in ascending order
+        System.out.println("The integers in acscending order are: ");
+        for (int i = 0; i < integers.length; i++) {
+            System.out.println(integers[i]);
+        }
     }
 }
