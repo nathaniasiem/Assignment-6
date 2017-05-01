0c622a49a3ff1b7d7d87b9276a97748735db146b
candy <candy@nsiem2k>
2017-05-01 08:36:45 -0400

    A6Q6-Completed with comments

--- src/A6Q6.java
+++ src/A6Q6.java
@@ -30,7 +30,34 @@ public class A6Q6 {
         for (int i = 0; i < marks.length; i++) {
             marks[i] = input.nextInt();
         }
-        
-        //
+
+        //find the lowest mark out of the group
+        //find the highest mark out of the group
+        //set variables to compare marks within array
+        int min = marks[0];
+        int max = marks[0];
+        //go through each mark
+        for (int i = 0; i < marks.length; i++) {
+            //condition when mark is larger
+            if (marks[i] > max) {
+                max = marks[i];
+                //condition when mark is lower
+            } else if (marks[i] < min) {
+                min = marks[i];
+            }
+        }
+        //print out lowest and highest mark
+        System.out.println("The highest mark is: " + max);
+        System.out.println("The lowest mark is: " + min);
+          
+        //calculate the sum of all marks
+        //use this to calculate the average
+        int sum=0;
+        for (int i =0;i<marks.length;i++){
+        sum=sum+marks[i];
+    }
+        //calculate the average value
+        double average=sum/marks.length;
+        System.out.println("The average mark in the class is: " +average+".");
     }
 }
