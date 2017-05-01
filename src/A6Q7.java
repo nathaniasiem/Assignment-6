66d6fb6fab40464a2d0f1ac8d74d6ad6f0a00af5
candy <candy@nsiem2k>
2017-05-01 08:52:56 -0400

    A6Q7-Initial commit

---
+++ src/A6Q7.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author candy
+ */
+public class A6Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create scanner to scan input
+        Scanner input =new Scanner(System.in);
+        
+        
+    }
+    
+}
