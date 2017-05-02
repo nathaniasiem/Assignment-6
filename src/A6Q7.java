8ffdb6123903edc58da82be6cec592a36390dda2
simon7323 <simon7323@HRH-IC0027346.wrdsb.ca>
2017-05-02 09:47:57 -0400

    A6Q7-completed with comments

--- src/A6Q7.java
+++ src/A6Q7.java
@@ -1,12 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author candy
@@ -18,9 +12,34 @@ public class A6Q7 {
      */
     public static void main(String[] args) {
         // create scanner to scan input
-        Scanner input =new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+
+        // ask user to input the maximum number in a set of numbers
+        System.out.println("Enter the maximum numbeer:");
+        int n = input.nextInt();
+
+        // create a list of numbers to determine prime numbers
+        //set variable to mark when true denotes prime number 
+        //assumes all numbers within the set are prime numbers
+        boolean[] isPrime = new boolean[n];
+        isPrime[0] = false;
+        for (int i = 1; i < n; i++) {
+            isPrime[i] = true;
+        }
+
         
-        
+        System.out.println("The prime numbers below " + n + " are: ");
+        //goes through each number in the set for prime numbers
+        for (int i = 2; i <= n; i++) {
+            //it is a prime number, when it is not marked 
+            if (isPrime[i - 1]) {
+                //print out the prime number
+                System.out.println(i);
+            }
+            //marks all of the numbers that are not prime
+            for (int j = i * i; j <= n; j += i) {
+                isPrime[j - 1] = false;
+            }
+        }
     }
-    
 }
